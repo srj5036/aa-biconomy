598ae89472cddf4ca4e57aeb9c81f6771ac7fd2d
Scott <srj5036@gmail.com>
2024-03-18 17:27:25 -0600

    fix names

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import { sendTransactionUsingSmartAccount } from './smartAccountUtils.js';
+import { sendSponsoredTransaction } from './biconomySmartAccount.js';
 
 const transaction = {
   to: "0x329Fb58Fe6706FB6945715287fbc39cFeC36654A",
   value: 100,
 };
 
-sendTransactionUsingSmartAccount(transaction);
+sendSponsoredTransaction(transaction);
